718291e257399ff12f32532a9a3277c8806ec54f
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2025-10-05 17:07:02 +0200

    tetss

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -1,10 +1,17 @@
 describe Opinion, type: :model do
-  describe 'associations' do
-    it { should belong_to(:user) }
+  describe 'validations' do
+    subject { described_class.new(content: 'Great product', mark: 5, user: build(:user)) }
+
+    it { is_expected.to validate_presence_of(:content) }
+    it { is_expected.to validate_inclusion_of(:mark).in_range(1..5) }
+
+    it { is_expected.to allow_value(1).for(:mark) }
+    it { is_expected.to allow_value(5).for(:mark) }
+    it { is_expected.not_to allow_value(0).for(:mark) }
+    it { is_expected.not_to allow_value(6).for(:mark) }
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:content) }
-    it { should validate_inclusion_of(:mark).in_range(1..5) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
   end
 end
