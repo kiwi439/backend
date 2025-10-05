de2e1b7463852ab1eb1a2c4ea9317f63782527dd
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2025-10-05 17:13:52 +0200

    fix

--- spec/models/products_order_spec.rb
+++ spec/models/products_order_spec.rb
@@ -1,10 +1,18 @@
 describe ProductsOrder, type: :model do
-  describe 'associations' do
-    it { should belong_to(:order) }
-    it { should belong_to(:product) }
+  subject do
+    described_class.new(
+      order: build(:order),
+      product: build(:product),
+      product_quantity: 2
+    )
   end
 
   describe 'validations' do
-    it { should validate_numericality_of(:product_quantity).only_integer }
+    it { is_expected.to validate_numericality_of(:product_quantity).only_integer }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:order) }
+    it { is_expected.to belong_to(:product) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 describe User, type: :model do
-  describe 'validations' do
-    subject { described_class.new(email: 'john.doe@example.com', password_digest: '$2a$12$YiA9qBzz8HWSXPf/WP1BkOfNFVB1zO2KY1wHbl1qD5dTdrKZzniq2') }
+  subject { described_class.new(email: 'john.doe@example.com', password: 'Password1!') }
 
-    it { should validate_uniqueness_of(:email).with_message('is already taken!') }
-    it { should allow_value('test@example.com').for(:email) }
-    it { should_not allow_value('test').for(:email) }
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of(:email).with_message('is already taken!') }
+    it { is_expected.to allow_value('test@example.com').for(:email) }
+    it { is_expected.not_to allow_value('test').for(:email) }
   end
 
   describe 'associations' do
-    it { should have_many(:opinions).dependent(:destroy) }
-    it { should have_many(:orders).dependent(:destroy) }
+    it { is_expected.to have_many(:opinions).dependent(:destroy) }
+    it { is_expected.to have_many(:orders).dependent(:destroy) }
   end
 end
