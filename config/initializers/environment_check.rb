e4c75c87d880e70cd0d248d4ff93833094d7dbd3
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2026-04-13 08:08:58 +0200

    Fix calling reusable workflow

--- config/initializers/environment_check.rb
+++ config/initializers/environment_check.rb
@@ -1,4 +1,3 @@
 Dotenv.load.each do |env_variable|
-  # TODO: For tests purposes - change it
-  # raise "Env variable: #{env_variable.first} not set!" unless env_variable.second.present?
+  raise "Env variable: #{env_variable.first} not set!" unless env_variable.second.present?
 end
