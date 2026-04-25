62218ec460ce3ca76e4067f049b06a2a075d0ca9
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2026-04-25 21:11:44 +0200

    tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,22 +1,24 @@
 describe Order, type: :model do
-  subject do
-    described_class.new(
-      name: 'John',
-      surname: 'Doe',
-      phone_number: '123456789',
-      street: 'Main street',
-      city: 'London',
-      postal_code: '34-300',
-      delivery_method: Order::DELIVERIES_DETAILS.dig(0, :method),
-      payment_method: Order::ALLOWED_PAYMENT_METHOD[0],
-      email: 'john.doe@example.com',
-      user: build(:user)
-    )
-  end
+  subject { build(:order) }
 
   describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:surname) }
+    it { is_expected.to validate_presence_of(:street) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:postal_code) }
+    it { is_expected.to validate_presence_of(:phone_number) }
+
     it { is_expected.to validate_inclusion_of(:delivery_method).in_array(Order::DELIVERIES_DETAILS.pluck(:method)) }
     it { is_expected.to validate_inclusion_of(:payment_method).in_array(Order::ALLOWED_PAYMENT_METHOD) }
+
+    it { is_expected.to allow_value('john.doe@example.com').for(:email) }
+    it { is_expected.not_to allow_value('test').for(:email) }
+
+    it { is_expected.to allow_value('34-300').for(:postal_code) }
+    it { is_expected.not_to allow_value('34300').for(:postal_code) }
+    it { is_expected.not_to allow_value('ab-cde').for(:postal_code) }
 
     it { is_expected.to allow_value('123456789').for(:phone_number) }
     it { is_expected.not_to allow_value('abc').for(:phone_number) }
@@ -28,7 +30,24 @@ describe Order, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_many(:products_orders).dependent(:destroy) }
+    it { is_expected.to have_many(:payments).dependent(:destroy) }
+  end
+
+  describe '#paid?' do
+    subject { order.paid? }
+
+    let(:order) { create(:order) }
+
+    it 'returns true when order has succeeded payment' do
+      create(:payment, order: order, status: :succeeded)
+      expect(subject).to be(true)
+    end
+
+    it 'returns false when order has no succeeded payments' do
+      create(:payment, order: order, status: :pending)
+      create(:payment, order: order, status: :failed)
+
+      expect(subject).to be(false)
+    end
   end
 end
-
-
