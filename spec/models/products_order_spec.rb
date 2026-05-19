fdf0616ac25a689434bb3112d6101bda5f3455b0
Michał Siwiec <michalsiwiec@192.168.1.109>
2026-05-19 08:04:04 +0200

    tests

--- spec/models/products_order_spec.rb
+++ spec/models/products_order_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe ProductsOrder, type: :model do
   subject do
     described_class.new(
@@ -14,5 +16,27 @@ describe ProductsOrder, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:order) }
     it { is_expected.to belong_to(:product) }
+  end
+
+  describe '#total_gross_price' do
+    subject { products_order.total_gross_price }
+
+    let(:product) { create(:product, price: 50.0, vat_rate: 23) }
+    let(:products_order) { create(:products_order, product:, product_quantity: 3) }
+
+    it 'returns product quantity multiplied by product gross price' do
+      expect(subject).to eq(184.5)
+    end
+  end
+
+  describe '#total_gross_price_cents' do
+    subject { products_order.total_gross_price_cents }
+
+    let(:product) { create(:product, price: 50.0, vat_rate: 23) }
+    let(:products_order) { create(:products_order, product:, product_quantity: 3) }
+
+    it 'returns total gross price in cents' do
+      expect(subject).to eq(18_450)
+    end
   end
 end
