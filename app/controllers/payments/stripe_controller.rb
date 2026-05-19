00013918466f8eb8204b7ea06d7aa3ea80a1de2d
Michał Siwiec <michalsiwiec@192.168.1.109>
2026-05-19 11:43:24 +0200

    fix tests for controller

--- app/controllers/payments/stripe_controller.rb
+++ app/controllers/payments/stripe_controller.rb
@@ -4,11 +4,13 @@ module Payments
   class StripeController < ApplicationController
     def update_status
       event = verify_event
-      handle_event(event)
-      head :ok
+      service = Payments::Stripe::HandleWebhookService.new(event: event)
+      service.call
+      return head :ok if service.success?
+
+      handle_error(service.errors.join(', '))
     rescue StandardError => e
-      Rollbar.error(e)
-      head :unprocessable_entity
+      handle_error(e)
     end
 
     private
@@ -19,43 +21,9 @@ module Payments
       ::Stripe::Webhook.construct_event(payload, signature, ENV.fetch('STRIPE_WEBHOOK_SIGNING_SECRET'))
     end
 
-    def handle_event(event)
-      session = event.data.object
-
-      case event.type
-      when 'checkout.session.completed'
-        payment = Payment.where(provider: 'stripe').find_by!("provider_data ->> ? = ?", 'checkout_session_id', session.id)
-        order = payment.order
-        status = Payment::STRIPE_STATUS_MAPPING.fetch(session.payment_status)
-
-        payment.update!(status: status)
-        invoice_created_successfully = create_invoice(order)
-        return unless invoice_created_successfully
-
-        send_order_created_email(order)
-      when 'checkout.session.expired'
-        payment = Payment.where(provider: 'stripe').find_by!("provider_data ->> ? = ?", 'checkout_session_id', session.id)
-        payment.expired!
-      else
-        Rollbar.error('Unsupported event type!', event_type: event.type)
-      end
-    end
-
-    def create_invoice(order)
-      service = Invoices::Infakt::CreateInvoiceService.new(order:)
-      response = service.call
-
-      if service.errors.any?
-        Rollbar.error(service.errors.join(', '))
-        return false
-      end
-
-      Invoice.create!(order:, provider_name: Invoice::INFAKT_PROVIDER, external_uuid: response['uuid'])
-      true
-    end
-
-    def send_order_created_email(order)
-      OrderMailer.with(order: order).order_created.deliver_later(queue: :order)
+    def handle_error(error)
+      Rollbar.error(error)
+      head :unprocessable_entity
     end
   end
 end
